d8e6021e0711436e2815f4bf3efeaee113e67662
raijinhasarrived <raijinhasarrived@gmail.com>
2023-12-30 08:21:37 +0500

    ☄️ fin backbtn

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,25 +1,25 @@
-// "use client";
+"use client";
 
-// import React from "react";
-// import { useBackButton } from "@tma.js/sdk-react";
+import React from "react";
+import { useBackButton } from "@tma.js/sdk-react";
 
-// import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function CreatePage() {
-  //   const backButton = useBackButton();
-  //   const router = useRouter();
+  const backButton = useBackButton();
+  const router = useRouter();
 
-  //   React.useEffect(() => {
-  //     const listener = () => router.push("/orders");
-  //     backButton.on("click", listener);
-  //     backButton.show();
+  React.useEffect(() => {
+    const listener = () => router.push("/orders");
+    backButton.on("click", listener);
+    backButton.show();
 
-  //     return () => {
-  //       backButton.off("click", listener);
-  //       backButton.hide();
-  //     };
-  //     // eslint-disable-next-line react-hooks/exhaustive-deps
-  //   }, []);
+    return () => {
+      backButton.off("click", listener);
+      backButton.hide();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return <div>crepate page</div>;
 }
--- app/help/page.tsx
+++ app/help/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import React from "react";
+import { useBackButton } from "@tma.js/sdk-react";
+
+import { useRouter } from "next/navigation";
+
 export default function HelpPage() {
+  const backButton = useBackButton();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const listener = () => router.push("/orders");
+    backButton.on("click", listener);
+    backButton.show();
+
+    return () => {
+      backButton.off("click", listener);
+      backButton.hide();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return <div>page</div>;
 }
