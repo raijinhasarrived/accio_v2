657fd13005b691e215bb21c49778e4b2521fdc4d
raijinhasarrived <raijinhasarrived@gmail.com>
2023-12-30 07:56:26 +0500

    ☄️ "(

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import React from "react";
+import { useBackButton } from "@tma.js/sdk-react";
+import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
+
 export default function CreatePage() {
+  const backButton = useBackButton();
+  const pathname = usePathname();
+  const router = useRouter();
+  React.useEffect(() => {
+    const listener = () => router.push("/orders");
+    backButton.on("click", listener);
+    pathname === "/orders" ? backButton.hide() : backButton.show();
+
+    return () => {
+      backButton.off("click", listener);
+      backButton.hide();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [backButton]);
+
+  console.log(pathname);
+
   return <div>crepate page</div>;
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -6,11 +6,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { I18nextProvider } from "react-i18next";
 
 import i18n from "@/lib/i18n";
-import { usePathname, useRouter } from "next/navigation";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  const pathname = usePathname();
-
   const client = new QueryClient({
     defaultOptions: {
       queries: {
@@ -24,22 +21,6 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
   React.useEffect(() => {
     i18n.changeLanguage(initData?.user?.languageCode);
   }, [initData]);
-
-  console.log(pathname);
-
-  const backButton = useBackButton();
-  const router = useRouter();
-  React.useEffect(() => {
-    const listener = () => router.push("/orders");
-    backButton.on("click", listener);
-    pathname === "/orders" ? backButton.hide() : backButton.show();
-
-    return () => {
-      backButton.off("click", listener);
-      backButton.hide();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [backButton]);
 
   return (
     <I18nextProvider i18n={i18n}>
