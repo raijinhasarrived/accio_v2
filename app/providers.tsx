d14b8baff5b12844bc01fa72f9db2e45681a4250
raijinhasarrived <raijinhasarrived@gmail.com>
2023-12-30 08:13:03 +0500

    ☄️ useEffect dep aray, triggers infinite rerendre of back btn

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,6 +6,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { I18nextProvider } from "react-i18next";
 
 import i18n from "@/lib/i18n";
+import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   const client = new QueryClient({
@@ -21,6 +23,22 @@ export const Providers = ({ children }: { children: React.ReactNode }) => {
   React.useEffect(() => {
     i18n.changeLanguage(initData?.user?.languageCode);
   }, [initData]);
+
+  const backButton = useBackButton();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  React.useEffect(() => {
+    const listener = () => router.push("/orders");
+    backButton.on("click", listener);
+    pathname === "/orders" ? backButton.hide() : backButton.show();
+
+    return () => {
+      backButton.off("click", listener);
+      backButton.hide();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <I18nextProvider i18n={i18n}>
