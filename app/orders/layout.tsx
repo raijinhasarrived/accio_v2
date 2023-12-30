283b0459a6e172f390fce28ac7fdaa1d8e60f938
raijinhasarrived <raijinhasarrived@gmail.com>
2023-12-30 05:22:00 +0500

    🇪🇷 wtf

--- app/orders/layout.tsx
+++ app/orders/layout.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import Link from "next/link";
-import { useThemeParams } from "@tma.js/sdk-react";
+import Lottie from "lottie-react";
+import { useLaunchParams, useThemeParams } from "@tma.js/sdk-react";
 import { useTranslation } from "react-i18next";
 
 import { OrderLinks } from "@/components/order-links";
 import { Button } from "@/components/ui/button";
+
+import DeniedAnim from "@/public/24 Prohibited.json";
 
 export default function OrdersLayout({
   children,
@@ -14,8 +17,19 @@ export default function OrdersLayout({
 }) {
   const { t, i18n } = useTranslation();
   const theme = useThemeParams();
-
+  const launchParams = useLaunchParams();
   const currentLanguage = i18n.language;
+
+  const paramList = launchParams.initDataRaw?.split("&")!;
+
+  let chatInstance: string | undefined;
+
+  for (const param of paramList) {
+    if (param.startsWith("chat_instance=")) {
+      chatInstance = param.slice("chat_instance=".length);
+      break;
+    }
+  }
 
   return (
     <div className="h-full w-full bg-background p-4">
@@ -60,7 +74,26 @@ export default function OrdersLayout({
           </Button>
         </Link>
       </div>
-      {children}
+      {chatInstance === "1490042131658129959" ? (
+        children
+      ) : (
+        <div
+          style={{ color: theme.textColor! }}
+          className="flex h-screen flex-col items-center justify-center gap-3 p-3"
+        >
+          <Lottie
+            loop={false}
+            className="h-44 w-44"
+            animationData={DeniedAnim}
+          />
+          <h1>{t("mainPage.msg")}</h1>
+          <a href="https://t.me/acciolog">
+            <Button style={{ backgroundColor: theme.buttonColor! }}>
+              {t("mainPage.action")}
+            </Button>
+          </a>
+        </div>
+      )}
     </div>
   );
 }
