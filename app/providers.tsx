4732d2380f836c642faa51efdb900e3f28f8a41b
raijinhasarrived <raijinhasarrived@gmail.com>
2023-12-30 08:15:10 +0500

    🇪🇷 next router

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,8 +6,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { I18nextProvider } from "react-i18next";
 
 import i18n from "@/lib/i18n";
-import { useRouter } from "next/router";
-import { usePathname } from "next/navigation";
+
+import { usePathname, useRouter } from "next/navigation";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   const client = new QueryClient({
