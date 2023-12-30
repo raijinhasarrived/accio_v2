da5e16ecb05a59a565e9159523565f2a9e50e72c
raijinhasarrived <raijinhasarrived@gmail.com>
2023-12-30 08:02:34 +0500

    🚑 pathname triggers infinite call in tg react

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -3,16 +3,16 @@
 import React from "react";
 import { useBackButton } from "@tma.js/sdk-react";
 
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function CreatePage() {
   const backButton = useBackButton();
-  const pathname = usePathname();
+
   const router = useRouter();
   React.useEffect(() => {
     const listener = () => router.push("/orders");
     backButton.on("click", listener);
-    pathname === "/orders" ? backButton.hide() : backButton.show();
+    backButton.show();
 
     return () => {
       backButton.off("click", listener);
@@ -20,8 +20,6 @@ export default function CreatePage() {
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [backButton]);
-
-  console.log(pathname);
 
   return <div>crepate page</div>;
 }
